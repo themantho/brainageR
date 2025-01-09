8828128d0361c3e96dd4acee1f1bd26ee21071cc
themantho <mia.anthonyjk@gmail.com>
2025-01-09 16:31:36 -0500

    Create run_dcm2bids.sh

---
+++ dcm2bids/run_dcm2bids.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# convert DICOMs to BIDS format
+
+subject=${1}
+subject_folder=${2}
+
+source $CONFIG_FILE
+
+dcm2bids -d $DICOM_DIR/$subject_folder -p $subject -s $ses -c $CONFIG_BIDS -o $BIDS_DIR --auto_extract_entities --force_dcm2bids
+
+
+
+
+
+
